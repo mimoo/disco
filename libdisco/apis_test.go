096b901566e17eb2d52166becfffed5d6bf5cc8c
david <david@matasano.com>
2017-11-15 16:39:20 +0000

    first commit for libdisco

---
+++ libdisco/apis_test.go
@@ -0,0 +1,69 @@
+package libdisco
+
+import (
+	"os"
+	"testing"
+)
+
+func TestCreationKeys(t *testing.T) {
+
+	// temporary files
+	discoKeyPairFile := "./discoKeyPairFile"
+	defer os.Remove(discoKeyPairFile)
+	rootPrivateKeyFile := "./rootPrivateKeyFile"
+	defer os.Remove(rootPrivateKeyFile)
+	rootPublicKeyFile := "./rootPublicKeyFile"
+	defer os.Remove(rootPublicKeyFile)
+
+	// Generate Disco Key pair
+	keyPair, err := GenerateAndSaveDiscoKeyPair(discoKeyPairFile)
+	if err != nil {
+		t.Error("Disco key pair couldn't be written on disk")
+		return
+	}
+	// Load Disco Key pair
+	keyPairTemp, err := LoadDiscoKeyPair(discoKeyPairFile)
+	if err != nil {
+		t.Error("Disco key pair couldn't be loaded from disk")
+		return
+	}
+	// compare
+	for i := 0; i < 32; i++ {
+		if keyPair.PublicKey[i] != keyPairTemp.PublicKey[i] ||
+			keyPair.PrivateKey[i] != keyPairTemp.PrivateKey[i] {
+			t.Error("Disco key pair generated and loaded are different")
+			return
+		}
+	}
+	// generate root key
+	err = GenerateAndSaveDiscoRootKeyPair(rootPrivateKeyFile, rootPublicKeyFile)
+	if err != nil {
+		t.Error("Disco key pair couldn't be written on disk")
+		return
+	}
+
+	// load private root key
+	rootPriv, err := LoadDiscoRootPrivateKey(rootPrivateKeyFile)
+	if err != nil {
+		t.Error("Disco root private key couldn't be loaded from disk")
+		return
+	}
+	// load public root key
+	rootPub, err := LoadDiscoRootPublicKey(rootPublicKeyFile)
+	if err != nil {
+		t.Error("Disco root public key couldn't be loaded from disk")
+		return
+	}
+
+	// create a proof
+	proof := CreateStaticPublicKeyProof(rootPriv, keyPair)
+
+	// verify the proof
+	verifior := CreatePublicKeyVerifier(rootPub)
+	if verifior(keyPair.PublicKey[:], proof) != true {
+		t.Error("cannot verify proof")
+		return
+	}
+
+	// end
+}
