e1db99bcb0583b9fa7f6b3726cb7323fdb8f18cd
david <david@matasano.com>
2017-11-30 15:02:19 +0000

    better documentation for library

--- web/discocrypto.com/src/components/library/DerivingKeys.vue
+++ web/discocrypto.com/src/components/library/DerivingKeys.vue
@@ -4,7 +4,9 @@
 
 		<h2><i class="fa fa-caret-right" aria-hidden="true"></i> Description</h2>
 
-		<pre><code>input := []byte("hi, how are you?")
+		<p>libdisco provides functions to "spread" a single key into several keys. The input key must be random and larger or equal to 128 bits (16 bytes) for security reasons. The following snippet takes a 16-byte key and derives two 32-byte keys:</p>
+
+		<pre><code>input, _ := hex.DecodeString("2825a87129f065748121cdf6c1dcf8b7")
 keys := libdisco.DeriveKeys(input, 64))
 key1 := keys[:32]
 key2 := keys[32:]</code></pre>
--- web/discocrypto.com/src/components/library/Hashing.vue
+++ web/discocrypto.com/src/components/library/Hashing.vue
@@ -4,8 +4,18 @@
 
 		<h2><i class="fa fa-caret-right" aria-hidden="true"></i> Description</h2>
 
+		<p>libdisco provides a simple hashing primitive that is not compatible with SHA-2 or SHA-3 or other hash functions. This means that the output you will get using libdisco's Hash function will be different. The use case and the security remain equivalent.</p>
+
+		<p>To obtain the 256-bit hash of some input, perform the following:</p>
+
 		<pre><code>input := []byte("hi, how are you?")
 digest := libdisco.Hash(input, 32))</code></pre>
+
+		<p>libdisco's hash function is more than just a hash function, it is actually an eXtendable Output Function (XOF) which provides a flexible output length. The minimum has been set to 32 bytes (256 bits) for security reasons, there is no practical maximum. The following example obtains an output of 1000 bytes:</p>
+
+		<pre><code>input := []byte("a very long output")
+digest := libdisco.Hash(input, 1000))</code></pre>
+
 
 	</section>
 
